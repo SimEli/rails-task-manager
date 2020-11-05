5c1b04d52eba118a0841af7b1148b047bd781b13
Simon Elicaste <simonelicaste@gmail.com>
2020-11-05 01:14:23 +0100

    rails task manager done, checked with solution but not able to add JS files and SCSS to review

---
+++ db/migrate/20201103142552_remove_completed_to_tasks.rb
@@ -0,0 +1,7 @@
+class RemoveCompletedToTasks < ActiveRecord::Migration[6.0]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+    #Ex:- :default =>''
+    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
+  end
+end
